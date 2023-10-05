7661583735a2e24418dd0bcdd32e3650da1b1e4d
chewbacca234 <carine.dupuis.es@gmail.com>
2023-10-05 17:29:04 +0200

    merge conflicts app resolved

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 require("dotenv").config();
 require("./models/connection");
 
-<<<<<<< HEAD
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var hashtagsRouter = require('./routes/hashtags');
-=======
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -14,7 +9,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var pwittsRouter = require("./routes/pwitts");
->>>>>>> 7b1c5378b73aeac4157f0b3c5430ec6d4621c92d
+var hashtagsRouter = require('./routes/hashtags');
 
 var app = express();
 
@@ -27,14 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-<<<<<<< HEAD
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hashtags', hashtagsRouter);
-=======
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/pwitts", pwittsRouter);
->>>>>>> 7b1c5378b73aeac4157f0b3c5430ec6d4621c92d
+app.use('/hashtags', hashtagsRouter);
 
 module.exports = app;
