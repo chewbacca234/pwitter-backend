474a8313e1aa60d6dfc7ad644e69a05ee7d7e17d
Your name <Your email>
2023-10-06 16:15:05 +0200

    modif userinfo

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const uid2 = require('uid2');
-const bcrypt = require('bcrypt');
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
-const User = require('../models/users');
-const Pwitt = require('../models/pwitts');
-const checkUserBody = require('../modules/checkUserBody');
+const User = require("../models/users");
+const Pwitt = require("../models/pwitts");
+const checkUserBody = require("../modules/checkUserBody");
 
 // Signup user
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
   // Check if require fields are completed
-  if (!checkUserBody(req.body, ['username', 'password', 'firstname'])) {
-    return res.status(400).json({ result: false, error: 'Missing or empty fields' });
+  if (!checkUserBody(req.body, ["username", "password", "firstname"])) {
+    return res
+      .status(400)
+      .json({ result: false, error: "Missing or empty fields" });
   }
 
   // Remove all space in username string
-  const username = req.body.username.replace(/ /g, '');
+  const username = req.body.username.replace(/ /g, "");
 
   // Remove start and end space in firstname string
   const firstname = req.body.firstname.trim();
 
   // Check if the user has not already been registered
-  User.findOne({ username: req.body.username }).then(foundUser => {
+  User.findOne({ username: req.body.username }).then((foundUser) => {
     if (!foundUser) {
       // Create new user
       const hash = bcrypt.hashSync(req.body.password, 10);
@@ -35,9 +37,19 @@ router.post('/signup', (req, res) => {
         likedList: [],
       });
 
-      newUser.save()
-        .then(user => {
-          return res.json({ result: true, token: user.token });
+      newUser
+        .save()
+        .then((user) => {
+          return res.json({
+            result: true,
+            userInfo: {
+              token: user.token,
+              firstname: user.firstname,
+              username: user.username,
+              picture: user.picture,
+              likedList: user.likedList,
+            },
+          });
         })
         .catch((error) => {
           // Set custom error for unique keys
@@ -47,69 +59,78 @@ router.post('/signup', (req, res) => {
           } else {
             errMsg = error.message;
           }
-          return res.status(400)
-            .json({ result: false, error: errMsg });
+          return res.status(400).json({ result: false, error: errMsg });
         });
     } else {
       // User already exists in database
-      return res.status(400).json({ result: false, error: 'User already exists' });
+      return res
+        .status(400)
+        .json({ result: false, error: "User already exists" });
     }
   });
 });
 
 // Signin user
-router.post('/signin', (req, res) => {
+router.post("/signin", (req, res) => {
   // Check if require fields are completed
-  if (!checkUserBody(req.body, ['username', 'password'])) {
-    return res.status(400).json({ result: false, error: 'Missing or empty fields' });
+  if (!checkUserBody(req.body, ["username", "password"])) {
+    return res
+      .status(400)
+      .json({ result: false, error: "Missing or empty fields" });
   }
 
   // Check if user exists
-  User.findOne({ username: req.body.username }).then(user => {
+  User.findOne({ username: req.body.username }).then((user) => {
     if (user && bcrypt.compareSync(req.body.password, user.password)) {
       // Signin user and return user's token
       return res.json({ result: true, token: user.token });
     } else {
       // Error message if not found
-      return res.status(400).json({ result: false, error: 'User not found or wrong password' });
+      return res
+        .status(400)
+        .json({ result: false, error: "User not found or wrong password" });
     }
   });
 });
 
 // Like a pwitt
-router.post('/addLike', (req, res) => {
+router.post("/addLike", (req, res) => {
   const { token, pwittId } = req.body;
   // Update user's liked list
   User.updateOne({ token }, { $push: { likedList: pwittId } })
-    .then(data => {
+    .then((data) => {
       if (data.modifiedCount) {
         // Pwitt added in like list
         return res.json({ result: true, pwittLiked: pwittId });
       } else {
         // Pwitt not added in like list
-        return res.status(400).json({ result: false, error: 'Pwitt not liked' });
+        return res
+          .status(400)
+          .json({ result: false, error: "Pwitt not liked" });
       }
     })
     // Update error
-    .catch(error => res.status(400).json({ result: false, error }));
+    .catch((error) => res.status(400).json({ result: false, error }));
 });
 
 // Unlike a pwitt
-router.post('/removeLike', (req, res) => {
+router.post("/removeLike", (req, res) => {
   const { token, pwittId } = req.body;
   // Update user's liked list
   User.updateOne({ token }, { $pull: { likedList: pwittId } })
-    .then(data => {
+    .then((data) => {
       if (data.modifiedCount) {
         // Pwitt removed in like list
         return res.json({ result: true, pwittUnliked: pwittId });
       } else {
         // Pwitt not removed in like list
-        return res.status(400).json({ result: false, error: 'Pwitt not unliked' });
+        return res
+          .status(400)
+          .json({ result: false, error: "Pwitt not unliked" });
       }
     })
     // Update error
-    .catch(error => res.status(400).json({ result: false, error }));
+    .catch((error) => res.status(400).json({ result: false, error }));
 });
 
 module.exports = router;
